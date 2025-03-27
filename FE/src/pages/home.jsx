ebd9d05fef8e4c1031f77e0727892cde7a2a49b0
AzuraAbi <herogia41@gmail.com>
2025-03-28 00:43:59 +0700

    bdau slide

--- FE/src/pages/home.jsx
+++ FE/src/pages/home.jsx
@@ -1,9 +1,22 @@
 import Navbar from "../components/navbar";
+import "../styles/home.css"
 
 function Home() {
     return (
         <>
             <Navbar />
+
+            <div className="banners">
+                <div className="banners__left">
+
+                </div>
+                <div className="banners__middle">
+
+                </div>
+                <div className="banners__right">
+                    
+                </div>
+            </div>
         </>
     )
 }
