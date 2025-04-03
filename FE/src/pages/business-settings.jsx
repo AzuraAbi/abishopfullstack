e98a0f1ab7409a3e3a97b5c8a65711c5d38db92e
AzuraAbi <herogia41@gmail.com>
2025-04-04 00:34:52 +0700

    k

--- FE/src/pages/business-settings.jsx
+++ FE/src/pages/business-settings.jsx
@@ -1,7 +1,36 @@
+import Navbar from "../components/navbar"
+import AccountTemplate from "../components/account"
+import "../styles/bussinesssettings.css"
+
+
 function BusinessSettings () {
     return (
         <>
-        
+            <Navbar />
+
+            <div className="business-page">
+                <div className="business-page__container">
+                    <AccountTemplate />
+                    
+                    <div className="business-page__content">
+                        <div className="business-page__title">Tài khoản doanh nghiệp</div>
+
+                        <div className="business-page__main">
+
+                            <div className="business__header">
+                                <div className="business__create-new">
+                                    Thêm sản phẩm
+                                </div>
+                            </div>
+
+                            <div className="busness__body">
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
